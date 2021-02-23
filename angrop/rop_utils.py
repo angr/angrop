169705b7650f19596af418933c57796eb0199236
Kyle ZENG <jkjh1jkjh1@gmail.com>
2021-02-23 14:05:08 -0700

    fix timeout stub

--- angrop/rop_utils.py
+++ angrop/rop_utils.py
@@ -232,18 +232,19 @@ class TimeoutError(Exception):
 def timeout(seconds_before_timeout):
     def decorate(f):
         def handler(signum, frame):
+            print("[angrop] Timeout")
             raise RopException("[angrop] Timeout!")
         def new_f(*args, **kwargs):
             old = signal.signal(signal.SIGALRM, handler)
             old_time_left = signal.alarm(seconds_before_timeout)
-            if 0 < old_time_left < second_before_timeout: # never lengthen existing timer
+            if 0 < old_time_left < seconds_before_timeout: # never lengthen existing timer
                 signal.alarm(old_time_left)
             start_time = time.time()
             try:
                 result = f(*args, **kwargs)
             finally:
                 if old_time_left > 0: # deduct f's run time from the saved timer
-                    old_time_left -= time.time() - start_time
+                    old_time_left -= int(time.time() - start_time)
                 signal.signal(signal.SIGALRM, old)
                 signal.alarm(old_time_left)
             return result
