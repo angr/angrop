96ce3a5e1c1845c57fede609b5fe8d006035a11a
Kevin Phoenix <kevin@kphoenix.us>
2022-03-23 19:11:37 -0700

    Update version to 9.2.0.dev0 [ci skip]

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='angrop',
-    version='9.1.gitrolling',
+    version='9.2.0.dev0',
     description='The rop chain builder based off of angr',
     packages=find_packages(),
     install_requires=[
         'tqdm',
-        'angr==9.1.gitrolling',
+        'angr==9.2.0.dev0',
     ],
 )
