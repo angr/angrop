1f3121f5b28f60d5748d37141e511e72d39da9cb
Kyle Zeng <zengyhkyle@gmail.com>
2023-03-04 14:27:00 -0700

    add a testcase for ignore_registers

--- tests/test_chainbuilder.py
+++ tests/test_chainbuilder.py
@@ -7,9 +7,6 @@ BIN_DIR = os.path.join(os.path.dirname(__file__), "..", "..", "binaries")
 CACHE_DIR = os.path.join(BIN_DIR, 'tests_data', 'angrop_gadgets_cache')
 
 def test_x86_64_func_call():
-    """
-    Ensure the chain executor is correct
-    """
     cache_path = os.path.join(CACHE_DIR, "1after909")
     proj = angr.Project(os.path.join(BIN_DIR, "tests", "x86_64", "1after909"), auto_load_libs=False)
     rop = proj.analyses.ROP()
@@ -52,6 +49,23 @@ def test_i386_syscall():
     chain =rop.do_syscall(4, [1, 0x80AC5E8, 17])
     state = chain.exec()
     assert state.posix.dumps(1) == b'/usr/share/locale'
+
+def test_ignore_regs():
+    cache_path = os.path.join(CACHE_DIR, "1after909")
+    proj = angr.Project(os.path.join(BIN_DIR, "tests", "x86_64", "1after909"), auto_load_libs=False)
+    rop = proj.analyses.ROP()
+
+    if os.path.exists(cache_path):
+        rop.load_gadgets(cache_path)
+    else:
+        rop.find_gadgets()
+        rop.save_gadgets(cache_path)
+
+    chain1 = rop.set_regs(rdi=0x402715)
+    chain2 = rop.func_call('puts', [0x402704], ignore_registers=['rdi'])
+    chain = chain1+chain2
+    state = chain.exec()
+    assert state.posix.dumps(1) == b'Failed to parse username.\n'
 
 def run_all():
     functions = globals()
