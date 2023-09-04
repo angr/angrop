826a2f655dfcea85bd4f6c97c854c882818036b6
Kyle Zeng <zengyhkyle@gmail.com>
2023-09-04 14:10:45 -0700

    oops

--- angrop/__init__.py
+++ angrop/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "9.2.9.dev0"
 
-import sys
-sys.set_int_max_str_digits(0)
-
 from . import rop
+
+import sys
+if hasattr(sys, "set_int_max_str_digits"): sys.set_int_max_str_digits(0)
+
