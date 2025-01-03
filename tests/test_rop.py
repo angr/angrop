f55bad755930479d1a7dd4a0c0f0c2faaa8877c7
Alexander Zhang <alex@alexyzhang.dev>
2025-01-02 21:30:11 -0800

    Update tests for conditional branches
    The gadget comparison code has to handle multiple gadgets at the same
    address due to conditional branches.

--- tests/test_rop.py
+++ tests/test_rop.py
@@ -72,7 +72,9 @@ def compare_gadgets(test_gadgets, known_gadgets):
     # check that each of the expected gadget addrs was found as a gadget
     # if it wasn't the best way to debug is to run:
     # angrop.gadget_analyzer.l.setLevel("DEBUG"); rop._gadget_analyzer.analyze_gadget(addr)
-    test_gadget_dict = {g.addr: g for g in test_gadgets}
+    test_gadget_dict = {}
+    for g in test_gadgets:
+        test_gadget_dict.setdefault(g.addr, []).append(g)
 
     found_addrs = set(g.addr for g in test_gadgets)
     for g in known_gadgets:
@@ -83,7 +85,13 @@ def compare_gadgets(test_gadgets, known_gadgets):
 
     # check gadgets
     for g in known_gadgets:
-        assert_gadgets_equal(g, test_gadget_dict[g.addr])
+        matching_gadgets = [
+            test_gadget
+            for test_gadget in test_gadget_dict[g.addr]
+            if test_gadget.bbl_addrs == g.bbl_addrs
+        ]
+        assert len(matching_gadgets) == 1
+        assert_gadgets_equal(g, matching_gadgets[0])
 
 
 def execute_chain(project, chain):
