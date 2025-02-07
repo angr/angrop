68f59f98a8c615388706dce6e3cba27686107f20
Kyle Zeng <zengyhkyle@gmail.com>
2025-02-06 18:20:15 -0700

    new testcases for ret2csu

--- tests/test_chainbuilder.py
+++ tests/test_chainbuilder.py
@@ -522,6 +522,36 @@ def test_aarch64_mem_access():
             assert action.addr.ast.concrete_value >= 0x1000
             assert action.addr.ast.concrete_value < 0x2000
 
+def test_mipstake():
+    proj = angr.Project(os.path.join(BIN_DIR, "tests", "mips", "mipstake"), auto_load_libs=True, arch="mips")
+    rop = proj.analyses.ROP(max_block_size=40)
+    g = rop.analyze_gadget(0x400E64) # lw $ra, 0x34($sp); lw $s5, 0x30($sp); lw $s4, 0x2c($sp); lw $s3, 0x28($sp); lw $s2, 0x24($sp); lw $s1, 0x20($sp); lw $s0, 0x1c($sp); jr $ra; addiu $sp, $sp, 0x38
+    assert g is not None
+
+    g = rop.analyze_gadget(0x400E40) # lw $t9, ($s1); addiu $s0, $s0, 1; move $a0, $s3; move $a1, $s4; jalr $t9; move $a2, $s5
+    assert g is not None
+    chain = rop.func_call("sleep", [0x41414141, 0x42424242, 0x43434343])
+    sleep_addr = proj.loader.main_object.imports['sleep'].value
+    state = chain.concrete_exec_til_addr(sleep_addr)
+    assert state.regs.a0.concrete_value == 0x41414141
+    assert state.regs.a1.concrete_value == 0x42424242
+    assert state.regs.a2.concrete_value == 0x43434343
+
+def test_unexploitable():
+    proj = angr.Project(os.path.join(BIN_DIR, "tests", "x86_64", "unexploitable"), auto_load_libs=False)
+    rop = proj.analyses.ROP(max_block_size=40, fast_mode=False, only_check_near_rets=False)
+    g = rop.analyze_gadget(0x4005D0) # mov rdx, r15; mov rsi, r14; mov edi, r13d; call qword ptr [r12 + rbx*8]
+    assert g is not None
+    g = rop.analyze_gadget(0x4005E6) # mov rbx, qword ptr [rsp + 8]; mov rbp, qword ptr [rsp + 0x10]; mov r12, qword ptr [rsp + 0x18]; mov r13, qword ptr [rsp + 0x20]; mov r14, qword ptr [rsp + 0x28]; mov r15, qword ptr [rsp + 0x30]; add rsp, 0x38; ret
+    assert g is not None
+    chain = rop.func_call("sleep", [0x41414141, 0x4242424242424242, 0x4343434343434343])
+
+    sleep_addr = proj.loader.main_object.imports['sleep'].value
+    state = chain.concrete_exec_til_addr(sleep_addr)
+    assert state.regs.rdi.concrete_value == 0x41414141
+    assert state.regs.rsi.concrete_value == 0x4242424242424242
+    assert state.regs.rdx.concrete_value == 0x4343434343434343
+
 def run_all():
     functions = globals()
     all_functions = {x:y for x, y in functions.items() if x.startswith('test_')}
--- tests/test_ropblock.py
+++ tests/test_ropblock.py
@@ -15,24 +15,6 @@ def test_ropblock():
     rb = RopBlock.from_gadget(gadget, rop.chain_builder._reg_setter)
     assert rb.next_pc_idx() == 11
 
-#0x0000000000036bb8 : mov rsp, r8 ; mov rbp, r9 ; jmp rdx
-#0x0000000000078765 : mov r8, rdx ; pop rbx ; ret
-#0x00000000000850a7 : mov rax, rdx ; ret
-#0x000000000007c980 : mov rax, rdi ; ret
-#0x00000000000f0e8e : mov rbx, rax ; mov rax, rbx ; pop rbx ; ret
-
-#def test_unexploitable():
-#    proj = angr.Project(os.path.join(BIN_DIR, "tests", "x86_64", "unexploitable"), auto_load_libs=False)
-#    rop = proj.analyses.ROP(max_block_size=40, fast_mode=False, only_check_near_rets=False)
-#    g = rop.analyze_gadget(0x4005D0) # mov rdx, r15; mov rsi, r14; mov edi, r13d; call qword ptr [r12 + rbx*8]
-#    assert g is not None
-#    g = rop.analyze_gadget(0x400512) # pop rbp ; ret
-#    assert g is not None
-#    g = rop.analyze_gadget(0x4005F5) # mov r13, qword ptr [rsp + 0x20]; mov r14, qword ptr [rsp + 0x28]; mov r15, qword ptr [rsp + 0x30]; add rsp, 0x38; ret
-#    assert g is not None
-#    chain = rop.set_regs(rdi=0, rsi=0x41414141, rdx=0x42424242)
-#    chain.print_payload()
-
 def test_reg_mover():
     cache_path = os.path.join(CACHE_DIR, "amd64_glibc_2.19")
     proj = angr.Project(os.path.join(BIN_DIR, "tests", "x86_64", "libc.so.6"), auto_load_libs=False)
