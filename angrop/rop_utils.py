6e43d9b75bf7188ff718712cf6108e6c7956d08e
Kyle Zeng <zengyhkyle@gmail.com>
2025-04-04 23:59:46 -0700

    update the timeout handler

--- angrop/rop_utils.py
+++ angrop/rop_utils.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import signal
 
@@ -372,6 +373,19 @@ def step_to_syscall(state):
 def timeout(seconds_before_timeout):
     def decorate(f):
         def handler(signum, frame):# pylint:disable=unused-argument
+            # Exception during __del__ will be ignore
+            # so if we happen to hit a __del__, retry the alarm
+            # reference: https://docs.python.org/3/reference/datamodel.html#object.__del__
+            while frame.f_back:
+                filename = frame.f_code.co_filename
+                if frame.f_code.co_name == '__del__':
+                    signal.setitimer(signal.ITIMER_REAL, 0.1, 0)
+                    return
+                if filename.startswith(sys.base_prefix) and filename.endswith("/weakref.py"):
+                    print("Delaying for 0.1s because of weakref.py")
+                    signal.setitimer(signal.ITIMER_REAL, 0.1, 0)
+                    return
+                frame = frame.f_back
             raise RopTimeoutException("[angrop] Timeout!")
         def new_f(*args, **kwargs):
             old = signal.signal(signal.SIGALRM, handler)
